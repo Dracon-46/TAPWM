83a2eedea80d624b4ddaa6f02f8f1ccdbe3c389a
Arthur Gaspare Camzano <87933696+Dracon-46@users.noreply.github.com>
2025-08-27 18:06:11 -0300

    Delete PWEBNODE/Exercicios directory

--- PWEBNODE/Exercicios/Exercicio4S.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');// carregando o módulo filesystem
-const data = fs.readFileSync('File.txt');
-// A execução é bloqueada aqui até o arquivo ser lido
-console.log(data.toString());
